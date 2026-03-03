5458c8cf492015ff38952f245626820d4dcd9c14
Fernando Sánchez Castilla <fernandosanchez@MacBook-Air-de-Fernando.local>
2026-03-03 12:17:41 +0100

    Añadida función dividir

--- Calculadora/Calculadora.cpp
+++ Calculadora/Calculadora.cpp
@@ -4,6 +4,7 @@ using namespace std;
 void mostrarMenu();
 double sumar(double a, double b);
 double restar(double a, double b);
+double dividir(double a, double b);
 int main() {
 	int opcion;
 	double num1, num2, resultado;
@@ -11,7 +12,7 @@ int main() {
 		mostrarMenu();
 		cout << "Elige una opcion: ";
 		cin >> opcion;
-		if (opcion >= 1 && opcion <= 2) {
+		if (opcion >= 1 && opcion <= 3) {
 			cout << "Introduce el primer numero: ";
 			cin >> num1;
 			cout << "Introduce el segundo numero: ";
@@ -25,6 +26,14 @@ int main() {
 				resultado = restar(num1, num2);
 				cout << "Resultado: " << resultado << endl;
 				break;
+			case 3:
+    			if (num2 != 0) {
+        	resultado = dividir(num1, num2);
+        	cout << "Resultado: " << resultado << endl;
+    } else {
+        cout << "Error: Division por cero" << endl;
+    }
+    break;
 			}
 		}
 		else if (opcion != 0) {
@@ -39,6 +48,7 @@ void mostrarMenu() {
 	cout << "=== CALCULADORA ===" << endl;
 	cout << "1. Sumar" << endl;
 	cout << "2. Restar" << endl;
+	cout << "3. Dividir" << endl; 
 	cout << "0. Salir" << endl;
 }
 double sumar(double a, double b) {
@@ -46,6 +56,9 @@ double sumar(double a, double b) {
 }
 double restar(double a, double b) {
 	return a - b;
+}
+double dividir(double a, double b) {
+    return a / b;
 }
 
 
